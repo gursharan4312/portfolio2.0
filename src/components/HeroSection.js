a04e498eb5477715813909fd491816961ff5f9fa
Gursharan Singh <gursharansingh4312@gmail.com>
2020-10-21 17:07:58 -0700

    nav fix

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -22,7 +22,11 @@ export default function HeroSection() {
             <ul id="navigation">
               <li>
                 <a href="/resume.pdf">Resume</a>
+              </li>
+              <li>
                 <a href="#projects">Projects</a>
+              </li>
+              <li>
                 <a href="#contact">Contact</a>
               </li>
             </ul>
