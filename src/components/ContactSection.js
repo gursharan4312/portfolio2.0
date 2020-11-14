1ac1e8829f58e67f757218067dd3b6e9730c3f7d
Gursharan Singh <gursharansingh4312@gmail.com>
2020-11-13 21:12:39 -0800

    form  changes

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,94 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import "./css/contactSection.scss";
 
 export default function ContactSection() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [formResponse, setFormResponse] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
+  // const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [formResponse, setFormResponse] = useState("");
 
-    let formdata = { name, email, subject, message };
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", formdata }),
-    })
-      .then((res) => {
-        setIsSubmitting(false);
-
-        if (res.status === 200) {
-          setFormResponse("<span class='success' >Message Submitted</span>");
-        } else {
-          setFormResponse("<span class='error' >Something went wrong</span>");
-        }
-      })
-      .catch((e) =>
-        setFormResponse("<span class='error' >Something went wrong</span>")
-      );
-  };
   return (
     <div className="contact-section" id="contact">
       <div className="container">
         <h3 style={{ textAlign: "center" }}>Get In Touch</h3>
-        <form name="contact" id="contact-form" onSubmit={handleSubmit}>
+        <form name="contact" id="contact-form" method="POST">
+          <input type="hidden" name="form-name" value="contact" />
           <label>
             Name:
-            <input
-              name="name"
-              value={name}
-              className="input-field"
-              onChange={(e) => setName(e.target.value)}
-            />
+            <input name="name" className="input-field" />
           </label>
           <label>
             Email:
-            <input
-              name="email"
-              type="email"
-              value={email}
-              className="input-field"
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <input name="email" type="email" className="input-field" />
           </label>
           <label>
             Subject:
-            <input
-              name="subject"
-              value={subject}
-              className="input-field"
-              onChange={(e) => setSubject(e.target.value)}
-            />
+            <input name="subject" className="input-field" />
           </label>
           <label>
             Message:
-            <textarea
-              name="message"
-              value={message}
-              className="input-field"
-              onChange={(e) => setMessage(e.target.value)}
-            />
+            <textarea name="message" className="input-field" />
           </label>
           <input id="submit-btn" type="submit" value="Send" />
-          {isSubmitting && <div className="loader" />}
+          {/* {isSubmitting && <div className="loader" />}
           {formResponse && (
             <div
               className="form-response"
               dangerouslySetInnerHTML={{ __html: formResponse }}
             />
-          )}
+          )} */}
         </form>
       </div>
     </div>
